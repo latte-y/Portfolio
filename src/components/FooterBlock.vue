e63bea05ecb889f751c131e54a45965b25329941
latte-y <yukimkyn1119@gmail.com>
2022-06-03 01:09:27 +0900

    一部を更新

---
+++ src/components/FooterBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>© 2022 Yuki Nakamura, All Rights Reserved.</p>
+  </footer>
+</template>
+
+<style scoped>
+p {
+  text-align: center;
+  background: rgb(233, 233, 233);
+  padding-top: 15px;
+  padding-bottom: 15px;
+  margin-bottom: 0;
+}
+</style>
